bb56f3841342ea4107b854efb013d2f11a2fe44f
Archit Khullar <akhullar@uncc.edu>
2018-11-08 08:19:07 -0500

    adding the ios app

---
+++ CarolinaFinTechApp/CarolinaFinTechApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarolinaFinTechApp
+//
+//  Created by Archit Khullar on 11/8/18.
+//  Copyright © 2018 ArchitKhullar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
